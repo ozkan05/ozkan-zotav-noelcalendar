44c069dda9c1d443c3c3359e9b75406051a40550
ozkan dagli <ozkan.dagli@saintmichelannecy.fr>
2026-01-30 00:07:54 +0100

    fini

--- calendar noel/Views/JourWindow.xaml.cs
+++ calendar noel/Views/JourWindow.xaml.cs
@@ -1,35 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace calendar_noel.Views
 {
-    /// <summary>
-    /// Logique d'interaction pour JourWindow.xaml
-    /// </summary>
     public partial class JourWindow : Window
     {
         public JourWindow(string imageUri)
         {
             InitializeComponent();
-            // Chargement de l’image depuis les ressources WPF
+
+            // Chargement de l’image
             CarteImage.Source = new BitmapImage(
                 new Uri(imageUri, UriKind.Absolute));
 
-            // Le texte a été retiré de la fenêtre (plus d'affichage de texte)
-            // CarteTexte remains in XAML but is not used.
+            // Chemin du fichier texte
+            string textFilePath = "messages.txt"; // adapte le chemin si nécessaire
+
+            try
+            {
+                if (File.Exists(textFilePath))
+                {
+                    // Lire toutes les lignes du fichier
+                    string[] lignes = File.ReadAllLines(textFilePath);
+
+                    if (lignes.Length > 0)
+                    {
+                        // Sélectionner une ligne au hasard
+                        Random rnd = new Random();
+                        int index = rnd.Next(lignes.Length);
+                        CarteTexte.Text = lignes[index];
+                    }
+                    else
+                    {
+                        CarteTexte.Text = "Le fichier est vide !";
+                    }
+                }
+                else
+                {
+                    CarteTexte.Text = "Fichier messages.txt introuvable !";
+                }
+            }
+            catch (Exception ex)
+            {
+                CarteTexte.Text = $"Erreur lors de la lecture : {ex.Message}";
+            }
         }
     }
 }
-
-
